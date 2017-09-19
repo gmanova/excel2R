cae6553c035f961a0d4fd5f4c5a0118453e564fc
Guy Manova <gmanova@gmanil.com>
2017-09-19 22:59:34 +0300

    pivot tables in R
    pivot data, pivot rows into columns, create "calculated fields" and
    filter/sort the pivot

---
+++ pivot tables.R
@@ -0,0 +1,96 @@
+# clean up the environment
+
+rm(list = ls())
+
+# we'll need dplyr and tidyr for this, by far easier than with base R
+
+library(dplyr)
+library(tidyr)
+
+# read in the Canadian SuperStore dataset from this dropbox address:
+
+df <- read.csv("C:/Users/User/Dropbox (Personal)/Personal/Courses/Excel2R/Dataframes/superstore.csv")
+write.csv(df, "superstore.csv")
+# just to be able to easily take a peak at the column names when you need to: 
+
+colnames <- colnames(df)
+colnames
+
+# with dplyr, pivoting data takes a very sql-like approach. let's say we wish to 
+# pivot so we see the total sales per product category, region and customer segment. 
+# in sql, it would be something like: 
+  # SELECT Product.Category, Region, Customer.Segment, SUM(Sales)
+  # FROM df
+  # GROUP BY Product.Category, Region, Customer.Segment
+
+# in dplyr, we follow a similar logic: 
+
+# first select the columns
+
+pivot <- df %>% 
+  select(Product.Category, Region, Customer.Segment, Sales)
+
+# then group by
+
+pivot <- df %>% 
+  select(Product.Category, Region, Customer.Segment, Sales)%>% 
+  group_by(Product.Category, Region, Customer.Segment)
+
+# then tell R how to sum the sales (sum, count, average etcs): 
+
+pivot <- df %>% 
+  select(Product.Category, Region, Customer.Segment, Sales)%>% 
+  group_by(Product.Category, Region, Customer.Segment) %>% 
+  summarise(TotalSales = sum(Sales))
+
+head(pivot)
+
+# and here's our pivot "select" statement, so to speak. 
+# let's add a few things, as we would in Excel pivot or sql - more computed columns,
+# adding avg sales and number (count) of sales transactions: 
+
+pivot <- df %>% 
+  select(Product.Category, Region, Customer.Segment, Sales)%>% 
+  group_by(Product.Category, Region, Customer.Segment) %>% 
+  summarise(TotalSales = sum(Sales), AvgSales = mean(Sales,na.rm = TRUE), 
+            NumSales = length(!is.na(Sales)))
+
+head(pivot) 
+# finally, let's add a calculated column from two other columns and calculate 
+# profitability. we'll add "Profit" to the summarize function, and use profit/sales
+# in the calculated column: 
+
+pivot <- df %>% 
+  select(Product.Category, Region, Customer.Segment, Sales, Profit)%>% 
+  group_by(Product.Category, Region, Customer.Segment) %>% 
+  summarise(TotalSales = sum(Sales), TotalProfit = sum(Profit), 
+            AvgSales = mean(Sales,na.rm = TRUE), 
+            NumSales = length(!is.na(Sales))) %>% 
+  mutate(AvgProfitMar = sum(TotalProfit)/TotalSales) %>% 
+  arrange(Region, desc(AvgSales))
+
+head(pivot)
+
+# let's emulate an Excel pivoting, or a sql "crosstab", by spreading out the  regions to 
+# seperate columns. let's take the original pivot with just one metric, TotalSales:
+
+  
+pivot2 <- df %>% 
+  select(Product.Category, Region, Customer.Segment, Sales) %>% 
+  group_by(Product.Category, Region, Customer.Segment) %>% 
+  summarise(TotalSales = sum(Sales))
+
+head(pivot2)
+
+# we use the TidyR "spread" function to summarize the region in columns: 
+
+pivot2 %>% 
+  spread(Region, TotalSales)
+
+# finally, let's subset with filters, like you would in an excel pivot when 
+# wanting to zero in on a particular category of data
+
+furniture <- pivot2 %>% 
+  filter(Product.Category=="Furniture")
+  
+furniture
